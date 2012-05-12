c686899d3c3504315c21526ab3531d5fa7287bab
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2012-05-12 20:41:27 +0200

    assertion helper for dumbster SmtpMessage

---
+++ src/main/java/pl/softmil/dumbster/SmtpMessageHelper.java
@@ -0,0 +1,37 @@
+package pl.softmil.dumbster;
+
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.Matcher;
+
+import com.dumbster.smtp.SmtpMessage;
+
+public class SmtpMessageHelper {
+    private final SmtpMessage smtpMessage;
+
+    public SmtpMessageHelper(SmtpMessage smtpMessage) {
+        super();
+        this.smtpMessage = smtpMessage;
+    }
+
+    public void assertBody(Matcher<String> matcher) {
+        assertThat("unable to match body", smtpMessage.getBody(), matcher);
+    }
+
+    public void assertTo(Matcher<String> matcher) {
+        assertThat("unable to match \"From\"", getHeaderValue("To"), matcher);
+    }
+
+    public void assertFrom(Matcher<String> matcher) {
+        assertThat("unable to match \"To\"", getHeaderValue("From"), matcher);
+    }
+
+    public void assertSubject(Matcher<String> matcher) {
+        assertThat("unable to match \"To\"", getHeaderValue("Subject"), matcher);
+    }
+
+    private String getHeaderValue(String headerName) {
+        return smtpMessage.getHeaderValue(headerName);
+    }
+
+}
