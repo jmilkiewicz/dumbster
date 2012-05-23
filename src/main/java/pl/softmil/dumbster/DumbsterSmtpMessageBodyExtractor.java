dba52ad7ac4d8d7bcd26ea9b1056fc4a61bc6ea9
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2012-05-23 17:19:56 +0200

    extracting links from messages

---
+++ src/main/java/pl/softmil/dumbster/DumbsterSmtpMessageBodyExtractor.java
@@ -0,0 +1,89 @@
+package pl.softmil.dumbster;
+
+import java.io.*;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeUtility;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.*;
+
+import com.dumbster.smtp.SmtpMessage;
+import com.google.common.io.CharStreams;
+
+public class DumbsterSmtpMessageBodyExtractor {
+    private static final String TEXT_HTML_CHARSET_UTF_8_CONTENT_TRANSFER_ENCODING = "text/html;charset=UTF-8Content-Transfer-Encoding:";
+    private final SmtpMessage smtpMessage;
+
+    public DumbsterSmtpMessageBodyExtractor(SmtpMessage smtpMessage) {
+        super();
+        this.smtpMessage = smtpMessage;
+    }
+
+    public String extractHtmlElementById(String id) {
+        String body = smtpMessage.getBody();
+        String quotedPrintable = digoutHtmlBody(body);
+        String contentTransferEncoding = digoutBodyContentTransferEncoding(body);
+        String decodeMessage = decodeMimeMessage(quotedPrintable,contentTransferEncoding);
+        Element elementById = getElementById(id, decodeMessage);
+        if(elementById == null){
+            throw new RuntimeException("element with id='"+ id +"' not found");
+        }
+        return elementById.attr("href");
+    }
+
+    private String digoutBodyContentTransferEncoding(String body) {
+        int startIndex = body.indexOf(TEXT_HTML_CHARSET_UTF_8_CONTENT_TRANSFER_ENCODING) + TEXT_HTML_CHARSET_UTF_8_CONTENT_TRANSFER_ENCODING.length();
+        int endIndex = body.indexOf("\n",startIndex); 
+        return body.substring(startIndex, endIndex).trim();
+    }
+
+    private Element getElementById(String id, String decodeMessage) {
+        Document parse = Jsoup.parse(decodeMessage);
+        Element elementById = parse.getElementById(id);
+        return elementById;
+    }
+
+    private String decodeMimeMessage(String body, String contentTransferEncoding) {
+        try {
+            return decodeMimeMessageUnsafe(body, contentTransferEncoding);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String decodeMimeMessageUnsafe(String htmlBody, String contentTransferEncoding)
+            throws MessagingException, IOException {
+        if(contentTransferEncoding.equals("quoted-printable")){
+            htmlBody = addCRLFAfter76(htmlBody);
+        }
+        InputStream is = new ByteArrayInputStream(
+                htmlBody.getBytes("iso-8859-1"));
+        InputStream decode = MimeUtility.decode(is, contentTransferEncoding);
+        String content = CharStreams.toString(new InputStreamReader(decode,
+                "iso-8859-1"));
+        return content;
+    }
+
+    private String digoutHtmlBody(String body) {
+        int indexOf = body.indexOf(TEXT_HTML_CHARSET_UTF_8_CONTENT_TRANSFER_ENCODING);
+        
+        int startPartIndex = body.indexOf("\n",indexOf) + "\n".length();              
+        int endIndex = body.indexOf("------=_Part", startPartIndex);
+        return body.substring(startPartIndex, endIndex);
+    }
+
+    private String addCRLFAfter76(String string) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < string.length(); i++) {
+            if (i > 0 && i % 76 == 0) {
+                result.append("\r\n");
+            }
+            result.append(string.charAt(i));
+        }
+        return result.toString();
+    }
+
+}
---
+++ src/test/java/pl/softmil/dumbster/DumbsterSmtpMessageBodyExtractorTest.java
@@ -0,0 +1,143 @@
+package pl.softmil.dumbster;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.junit.*;
+import org.junit.rules.ExpectedException;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.javamail.*;
+
+import pl.softmil.dumbster.junit.StartStopDumbsterRule;
+
+import com.dumbster.smtp.SmtpMessage;
+
+public class DumbsterSmtpMessageBodyExtractorTest {
+    private static final int SMTP_PORT = 9900;
+    private static JavaMailSender javaMailSender;
+
+    @BeforeClass
+    public static void buildMailSender() {
+        JavaMailSenderImpl senderImpl = new JavaMailSenderImpl();
+        senderImpl.setDefaultEncoding("UTF-8");
+        senderImpl.setPort(SMTP_PORT);
+        senderImpl.setHost("localhost");
+        javaMailSender = senderImpl;
+    }
+
+    @ClassRule
+    public static StartStopDumbsterRule startStopDumbster = new StartStopDumbsterRule(
+            SMTP_PORT);
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void testExtractLinkByIdFromHtmlEmailWithAttachment()
+            throws MessagingException {
+        String emailBody = "<html><body><img src='cid:financeRingLogo'><br/>Zarejestruj sie przez kliknięcie na: <a id=\"registration-link\" href=\"http://www.onet.pl?costam=aaa&asas=333\">http://www.onet.pl?costam=aaa&asas=333</a></body></html>";
+        sendEmailWithAttachement(emailBody);
+
+        SmtpMessage aSingleMessageReceived = new SimpleSmtpServerHelper(
+                startStopDumbster.getSimpleSmtpServer())
+                .aSingleMessageReceived();
+        DumbsterSmtpMessageBodyExtractor dumbsterSmtpMessageBodyExtractor = new DumbsterSmtpMessageBodyExtractor(
+                aSingleMessageReceived);
+        assertThat(
+                dumbsterSmtpMessageBodyExtractor
+                        .extractHtmlElementById("registration-link"),
+                equalTo("http://www.onet.pl?costam=aaa&asas=333"));
+    }
+
+    @Test
+    public void testExtractLinkByIdFromNonHtmlMessage()
+            throws MessagingException {
+        String emailBody = "http://www.onet.pl?costam=aaa&asas=333";
+        sendEmailNoAttachement(emailBody);
+
+        SmtpMessage aSingleMessageReceived = new SimpleSmtpServerHelper(
+                startStopDumbster.getSimpleSmtpServer())
+                .aSingleMessageReceived();
+        DumbsterSmtpMessageBodyExtractor dumbsterSmtpMessageBodyExtractor = new DumbsterSmtpMessageBodyExtractor(
+                aSingleMessageReceived);
+        exception.expect(RuntimeException.class);
+        dumbsterSmtpMessageBodyExtractor
+                .extractHtmlElementById("registration-link");
+    }
+
+    @Test
+    public void testExtractLinkByIdFrom7bitHtml() throws MessagingException {
+        String emailBody = "<html><body><br/>Click on <a id=\"weird_id\" href=\"http://www.onet.pl?costam=aaa&asas=333\">http://www.onet.pl?costam=aaa&asas=333</a></body></html>";
+        sendEmailNoAttachement(emailBody);
+
+        SmtpMessage aSingleMessageReceived = new SimpleSmtpServerHelper(
+                startStopDumbster.getSimpleSmtpServer())
+                .aSingleMessageReceived();
+        DumbsterSmtpMessageBodyExtractor dumbsterSmtpMessageBodyExtractor = new DumbsterSmtpMessageBodyExtractor(
+                aSingleMessageReceived);
+        assertThat(
+                dumbsterSmtpMessageBodyExtractor
+                        .extractHtmlElementById("weird_id"),
+                equalTo("http://www.onet.pl?costam=aaa&asas=333"));
+    }
+
+    @Test
+    public void testExtractLinkByIdFrom7bitHtmlWhenNoGivenElementIdExists()
+            throws MessagingException {
+        String emailBody = "<html><body><br/>Click on <a id=\"foo_id\" href=\"http://www.onet.pl?costam=aaa&asas=333\">http://www.onet.pl?costam=aaa&asas=333</a></body></html>";
+        sendEmailNoAttachement(emailBody);
+
+        SmtpMessage aSingleMessageReceived = new SimpleSmtpServerHelper(
+                startStopDumbster.getSimpleSmtpServer())
+                .aSingleMessageReceived();
+        DumbsterSmtpMessageBodyExtractor dumbsterSmtpMessageBodyExtractor = new DumbsterSmtpMessageBodyExtractor(
+                aSingleMessageReceived);
+        exception.expect(RuntimeException.class);
+        dumbsterSmtpMessageBodyExtractor.extractHtmlElementById("bar_id");
+    }
+
+    @Test
+    public void testExtractLinkByIdFromHtmlEmailWithNoAttachment()
+            throws MessagingException {
+        String emailBody = "<html><body><br/>Zarejestruj się przez kliknięcie na: <a id=\"registration-link\" href=\"http://www.onet.pl?costam=aaa&asas=333\">http://www.onet.pl?costam=aaa&asas=333</a></body></html>";
+        sendEmailNoAttachement(emailBody);
+
+        SmtpMessage aSingleMessageReceived = new SimpleSmtpServerHelper(
+                startStopDumbster.getSimpleSmtpServer())
+                .aSingleMessageReceived();
+        DumbsterSmtpMessageBodyExtractor dumbsterSmtpMessageBodyExtractor = new DumbsterSmtpMessageBodyExtractor(
+                aSingleMessageReceived);
+        assertThat(
+                dumbsterSmtpMessageBodyExtractor
+                        .extractHtmlElementById("registration-link"),
+                equalTo("http://www.onet.pl?costam=aaa&asas=333"));
+    }
+
+    private void sendEmailWithAttachement(String emailBody)
+            throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom("from@bar.foo");
+        helper.setTo("jmil@test.bar");
+        helper.setSubject("sample subject");
+        helper.setText(emailBody, true);
+        ClassPathResource image = new ClassPathResource("icon.png");
+        helper.addInline("financeRingLogo", image);
+        javaMailSender.send(message);
+    }
+
+    private void sendEmailNoAttachement(String emailBody)
+            throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom("from@bar.foo");
+        helper.setTo("jmil@test.bar");
+        helper.setSubject("sample subject");
+        helper.setText(emailBody, true);
+        javaMailSender.send(message);
+    }
+
+}
